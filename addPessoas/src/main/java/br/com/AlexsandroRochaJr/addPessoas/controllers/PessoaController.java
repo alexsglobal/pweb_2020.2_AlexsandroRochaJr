9427a4c044c52d040c6eb8610a98a4140c0b8c04
alexsglobal <grazylexs4@gmail.com>
2021-04-06 14:44:44 -0300

    Colocando MVC no projeto

---
+++ addPessoas/src/main/java/br/com/AlexsandroRochaJr/addPessoas/controllers/PessoaController.java
@@ -0,0 +1,5 @@
+package br.com.AlexsandroRochaJr.addPessoas.controllers;
+
+public class PessoaController {
+
+}
---
+++ addPessoas/src/main/java/br/com/AlexsandroRochaJr/addPessoas/repositories/PessoaRepository.java
@@ -0,0 +1,5 @@
+package br.com.AlexsandroRochaJr.addPessoas.repositories;
+
+public class PessoaRepository {
+
+}
