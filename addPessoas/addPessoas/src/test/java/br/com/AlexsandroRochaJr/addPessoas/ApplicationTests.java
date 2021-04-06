93f7b55ee5329257e15f834111ff315cb5990893
alexsglobal <grazylexs4@gmail.com>
2021-04-06 12:36:58 -0300

    Iniciando CRUD da unidade1

---
+++ addPessoas/addPessoas/src/test/java/br/com/AlexsandroRochaJr/addPessoas/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.AlexsandroRochaJr.addPessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
