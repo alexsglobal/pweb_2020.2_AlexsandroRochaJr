81ae166ced59b8400af8a8d7f1bd663d1fa5b88e
alexsglobal <grazylexs4@gmail.com>
2021-04-12 13:27:00 -0300

    mudanças sem o addPessoas

--- addPessoas/src/main/java/br/com/AlexsandroRochaJr/addPessoas/controllers/PessoaController.java
+++ addPessoas/src/main/java/br/com/AlexsandroRochaJr/addPessoas/controllers/PessoaController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -21,10 +23,12 @@ public class PessoaController {
 	
 	
 	
-	@GetMapping("/index.html")
+	@GetMapping("/")
 	public String index () {
 	 return "index.html";
 }
+
+
 	//as 2 formas possíveis de ser acessado o menu "listar pessoas"
 	@GetMapping("/listarPessoas")
 	public ModelAndView listarPessoas () {
@@ -33,16 +37,19 @@ public class PessoaController {
 	mav.addObject("pessoas", lista);
 	return mav;
 	}
-	@GetMapping("/listarPessoas.html")
-	public ModelAndView listarPessoascomhtml () {
-		List<Pessoa> lista = pessoaRepo.findAll();
-	ModelAndView mav = new ModelAndView("listarPessoas");
-	mav.addObject("pessoas", lista);
-	return mav;
+	@PostMapping("/editar/{id}")
+	public ModelAndView editarPessoa(@PathVariable("id")long id, Pessoa pessoa) {
+	pessoaRepo.save(pessoa);
+	return new ModelAndView("redirect:/listarPessoas");
 	}
 	
-
+@GetMapping("/remover/{id}")
+public ModelAndView removerPessoa(@PathVariable("id")long id) {
+	Pessoa aRemover = pessoaRepo.findById(id)
+	.orElseThrow(()	-> new IllegalArgumentException("ID inválido:" + id ));
 	
+	pessoaRepo.delete(aRemover);
+	return new ModelAndView("redirect: /listarPessoas");
 	}
-	
+}
 	
